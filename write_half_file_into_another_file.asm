b0f757464540d4c3d3ea37e476a40382ab107160
崔晋瑜 <2510271615@qq.com>
2018-06-19 18:31:01 +0800

    undone

--- write_half_file_into_another_file.asm
+++ write_half_file_into_another_file.asm
@@ -1,9 +1,9 @@
 data segment
-	file db 'a\text.txt',0
-	buf db 100,?,100 dub(?)
+	file db 'text.txt',0
+	buf db 100 dup(?)
 	fh dw ?
 
-	copyfile db 'a\copytext.txt',0
+	copyfile db 'copytext.txt',0
 	copyfh dw ?
 	error_msg db 0dh,0ah,'error','$'
 
@@ -20,11 +20,32 @@ code segment
 	mov ah,3fh
 	mov bx,fh
 	mov cx,512
-	lea lea,buf
+	lea dx,buf
 	int 21h
 	jc error
 	cmp ax,0
+	mov fh,ax
 	je exit
+	
+write:
+	;create file
+	mov ah,3ch
+	mov cx,00
+	lea dx,copyfile
+	int 21h
+	jc error
+	;write to the file
+	mov ax,fh
+	mov bx,2
+	div bx
+	mov cx,ax
+	mov ah,40h
+	mov bx,copyfh
+	lea dx,buf
+	int 21h
+	jc error
+	jmp exit
+	
 	
 error:
 	lea dx,error_msg
@@ -36,3 +57,4 @@ exit:
 	int 21h
 	
 code ends
+end
